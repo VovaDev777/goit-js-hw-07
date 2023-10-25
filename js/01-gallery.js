5e282444f055587d7f96e17cafa065e3da5ea28f
VovaDev777 <polvovater@gmail.com>
2023-10-25 23:32:29 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,12 +8,15 @@ galleryItems.forEach((image) => {
     const listItem = document.createElement("li");
     listItem.insertAdjacentHTML(
       "beforeend",
-      `<img class = "image-animation" src="${image.preview}" alt="${image.description}" width = "100%" height = "100%"/>`
+      `<a class="gallery__link" href="large-image.jpg">
+      <img class = "gallery__image" src="${image.preview}" data-source="large-image.jpg" alt="${image.description}" width = "100%" height = "100%"/>
+      </a>`
     );
-    
+    listItem.classList.add('gallery__item');
     
 
-    listItem.addEventListener('click', () => {
+    listItem.addEventListener('click', (event) => {
+      event.preventDefault();
         const lightbox = basicLightbox.create(`
           <img src="${image.original}" alt="${image.description}" width="100%" height="100%"/>
         `);
